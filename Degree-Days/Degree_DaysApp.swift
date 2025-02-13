69102fc6939c652051f82e6ddcdf75e1500c0303
cbalesohay <kawipro32@gmail.com>
2025-02-13 15:00:18 -0800

    Initial Commit

---
+++ Degree-Days/Degree_DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Degree_DaysApp.swift
+//  Degree-Days
+//
+//  Created by Chris Bailey on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Degree_DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
